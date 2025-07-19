2ce83ba524925b18941d0fe834944db443ede402
akaryaka <akariaka@yandex.ru>
2025-07-20 01:18:27 +0600

    add 2 folder

---
+++ 2/app.js
@@ -0,0 +1,15 @@
+// Function Declaration.
+function greet(name) {
+  console.log("Привет, " + name + "!");
+};
+
+// Function Expression.
+const greet = function(name) {
+  console.log("Привет, " + name + "!");
+};
+
+// Стрелочная функция
+// Пример стрелочной функции
+const sum = (a, b) => {
+  return a + b;
+};
